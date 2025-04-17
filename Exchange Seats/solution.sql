754cf3a1f82ac613f3ff9b877b0d23139426fd88
hmulavekar <hmulavekar@gmail.com>
2025-04-17 00:40:32 -0600

    added solution to more problems

---
+++ Exchange Seats/solution.sql
@@ -0,0 +1,10 @@
+/* Write your PL/SQL query statement below */
+
+SELECT 
+    CASE WHEN MOD(ID,2) = 0 THEN ID - 1
+         WHEN MOD(ID,2) = 1 AND ID <> (SELECT MAX(ID) FROM SEAT) THEN ID+1 
+    ELSE ID END  AS ID,
+    STUDENT
+FROM SEAT
+ORDER BY ID ;
+
